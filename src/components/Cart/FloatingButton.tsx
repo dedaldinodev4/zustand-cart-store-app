a3f5347cab9d4adb171a63e4cce8d2040339f41d
Dedaldino Daniel <dedaldinodev4@gmail.com>
2025-10-24 19:11:17 +0100

    feat: FloatingCartButton

---
+++ src/components/Cart/FloatingButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useCartStore } from "@/store/cartStore";
+import { useUIStore } from "@/store/uiStore";
+import { ShoppingCart } from "lucide-react";
+
+export const FloatingCartButton = () => {
+  const { items, total } = useCartStore();
+  const { toggleCart } = useUIStore();
+
+  const itemCount = items.reduce((acc, item) => acc + item.quantity, 0);
+
+  return (
+    <button
+      onClick={toggleCart}
+      className="fixed bottom-6 right-6 bg-blue-600 text-white rounded-full shadow-lg flex items-center gap-3 px-4 py-3 hover:bg-blue-700 transition-all duration-200 ease-in-out hover:scale-105 z-50"
+    >
+      <ShoppingCart size={22} />
+      <div className="text-sm font-semibold">
+        {itemCount} item{itemCount > 1 ? "s" : ""}
+      </div>
+      <span className="bg-white text-blue-700 text-xs px-2 py-1 rounded-md font-bold">
+        ${total.toFixed(2)}
+      </span>
+    </button>
+  );
+}
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,10 +3,13 @@ import { ProductCard } from "@/components/Common/ProductCard";
 import { Header } from "@/components/Common/Header";
 import { useProducts } from "@/hooks/products/useProducts";
 import { CartDrawer } from "@/components/Cart/CartDrawer";
+import { useCartStore } from "@/store/cartStore";
+import { FloatingCartButton } from "@/components/Cart/FloatingButton";
 
 
 const Home = () => {
   const { data, isLoading, isError } = useProducts();
+  const { items } = useCartStore()
 
   if (isLoading) return <p className="text-center mt-10">Loading products...</p>;
   if (isError) return <p className="text-center mt-10 text-red-500">Error loading products!</p>;
@@ -15,6 +18,7 @@ const Home = () => {
     <main>
       <Header />
       <CartDrawer />
+      {items && <FloatingCartButton />}
       <div className="max-w-7xl mx-auto p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {data.map((product: any) => (
           <ProductCard key={product.id} product={product} />
